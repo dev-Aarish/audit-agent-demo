58276f228edd362375b487ff921d4e4311bb85d8
Rimmon Bhowmick <rimmonbhowmick@gmail.com>
2026-03-13 14:54:03 +0530

    PDF Download

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 
 /*
 ----------------------------------
